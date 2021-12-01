4a6470b95785e49fed24d3fa404d86af40b9860b
Pia Gerhofer <pia.gerhofer@teamecho.at>
2021-12-01 18:12:34 +0100

    Day 1

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,3 +1,31 @@
-fun main () {
-    println("Hello AOC")
+object Day1 {
+
+    fun part1(filePath: String) {
+        val numbers = javaClass.getResource(filePath).readText()
+            .split("\n")
+            .map { line -> line.trim().toInt() }
+
+        val increases = numbers
+            .filterIndexed { idx, current -> (idx + 1) < numbers.size && current < numbers[idx+1]}
+            .count()
+
+        println(increases)
+    }
+
+    fun part2(filePath: String) {
+        val numbers = javaClass.getResource(filePath).readText()
+            .split("\n")
+            .map { line -> line.trim().toInt() }
+
+        val increases = numbers
+            .filterIndexed { idx, _ -> (idx + 3) < numbers.size && numbers.sumOfThree(idx) < numbers.sumOfThree(idx+1)}
+            .count()
+
+        println(increases)
+    }
+
+}
+
+fun List<Int>.sumOfThree(start: Int): Int {
+    return this[start] + this[start + 1] + this[start + 2]
 }
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main () {
+
+    println("Day 1")
+    Day1.part1("day1.txt")
+    Day1.part2("day1.txt")
+
+}
