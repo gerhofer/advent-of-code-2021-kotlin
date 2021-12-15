9838c8c72d790d9155beb0aaf04b2f814ac4445d
Pia Gerhofer <pia.gerhofer@teamecho.at>
2021-12-15 18:27:48 +0100

    Day 15 second try

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -1,68 +1,64 @@
 object Day15 {
 
     fun part1(filePath: String) {
-        val chitonDensities = javaClass.getResource(filePath).readText()
+        val densities = javaClass.getResource(filePath).readText()
             .split("\r\n")
-            .map { line -> line.split("")
-                .filter { it.isNotEmpty() }
-                .map { chitonDensity -> chitonDensity.toInt() }
+            .map { line ->
+                line.split("")
+                    .filter { it.isNotEmpty() }
+                    .map { chitonDensity ->
+                        ChitonDensity(chitonDensity.toInt())
+                    }
             }
 
-        val paths = mutableListOf<Path>()
-        val pathWithStartingPoint = Path(
-            riskLevel = 0,
-            steps = mutableListOf(Point(0, 0))
-        )
+        densities[0][0].optimumPath = Path(0, mutableListOf(Point(0, 0)))
 
-        getShortestPath(chitonDensities, pathWithStartingPoint, paths)
+        for (y in (densities.indices)) {
+            for (x in (densities.indices)) {
+                if (y == 0 && x == 0) {
+                    continue
+                }
+                val fromLeft = pathFromLeft(densities, x, y)
+                val fromTop = pathFromTop(densities, x, y)
+                if (fromLeft != null && (fromTop == null || fromLeft.riskLevel < fromTop.riskLevel)) {
+                    densities[y][x].optimumPath = fromLeft
+                } else {
+                    densities[y][x].optimumPath = fromTop
+                }
+            }
+        }
 
-        println(paths.minOf { it.riskLevel })
+        println(densities.joinToString("\n") { line ->
+            line.joinToString("") {
+                String.format(
+                    "%4d",
+                    it.optimumPath?.riskLevel ?: -1
+                )
+            }
+        })
+
     }
 
-    fun getShortestPath(area: List<List<Int>>, currentPath: Path, existingPaths: MutableList<Path>) {
-        val last = currentPath.steps.last()
-        if (last.x == 0 && last.y == 0) {
-            println("added path $currentPath")
-            existingPaths.add(currentPath)
-            return
-        }
-
-        if (existingPaths.any { it.riskLevel <= currentPath.riskLevel }) {
-            return
-        }
-
-        val neighbours = getPossibleNeighbours(area, currentPath.steps.last())
-            .filter { it !in currentPath.steps }
-
-        for (neighbour in neighbours.sortedBy { area[it.y][it.x] }) {
-            val newCurrentPath = currentPath.copy(
-                riskLevel = currentPath.riskLevel + area[neighbour.y][neighbour.x],
-                steps = currentPath.steps.plus(neighbour).toMutableList()
+    fun pathFromLeft(densities: List<List<ChitonDensity>>, x: Int, y: Int): Path? {
+        if (x > 0) {
+            val path = densities[y][x - 1].optimumPath ?: error("should not happen")
+            return path.copy(
+                riskLevel = path.riskLevel + densities[y][x].value,
+                steps = path.steps.plus(Point(x, y)).toMutableList()
             )
-
-            getShortestPath(area, newCurrentPath, existingPaths)
         }
-
+        return null
     }
 
-    fun getPossibleNeighbours(area: List<List<Int>>, currentPoint: Point) : List<Point> {
-        val neighbors = mutableListOf<Point>()
-        if (currentPoint.x > 0) {
-            neighbors.add(currentPoint.left())
+    fun pathFromTop(densities: List<List<ChitonDensity>>, x: Int, y: Int): Path? {
+        if (y > 0) {
+            val path = densities[y-1][x].optimumPath ?: error("should not happen")
+            return path.copy(
+                riskLevel = path.riskLevel + densities[y][x].value,
+                steps = path.steps.plus(Point(x, y)).toMutableList()
+            )
         }
-
-        //if (currentPoint.x < area.size - 1) {
-        //    neighbors.add(currentPoint.right())
-        //}
-
-        if (currentPoint.y > 0) {
-            neighbors.add(currentPoint.top())
-        }
-
-        //if (currentPoint.y < area.size - 1) {
-        //    neighbors.add(currentPoint.bottom())
-        //}
-        return neighbors.toList()
+        return null
     }
 
     fun part2(filePath: String) {
@@ -76,7 +72,7 @@ object Day15 {
 
     data class ChitonDensity(
         val value: Int,
-        var excluded: Boolean = false
+        var optimumPath: Path? = null
     )
 
     data class Point(
@@ -87,7 +83,7 @@ object Day15 {
         fun left(): Point = Point(this.x - 1, this.y)
         fun top(): Point = Point(this.x, this.y - 1)
         fun right(): Point = Point(this.x + 1, this.y)
-        fun bottom(): Point = Point(this.x , this.y + 1)
+        fun bottom(): Point = Point(this.x, this.y + 1)
 
     }
 
